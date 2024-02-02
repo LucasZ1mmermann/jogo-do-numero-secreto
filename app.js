269dcfde6abd250a6275c682dd1a933060f50392
LucasZ1mmermann <150888012+LucasZ1mmermann@users.noreply.github.com>
2024-02-02 13:06:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function mensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
-mensagemInicial();-
+mensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
